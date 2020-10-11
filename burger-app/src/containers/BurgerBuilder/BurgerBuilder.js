b090353a35606676a546f81ae1387fce4607f105
usamahassankhan <usamahassankhan870@gmail.com>
2020-10-11 23:33:39 +0500

    burger images

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import Aux from '../../hoc/Auxilary';
+import Burger from '../../components/Burger/Burger';
  class BurgerBuilder extends Component {
     render() {
         return (
             <Aux>
-            <div>
-                Burger
-            </div>
-            <div>Build Controls</div>
+         
+              <div><Burger/></div>  
+            
+            <div>Build Controls</div>  
 
             </Aux>  );
     }
 }
-
+ 
 export default  BurgerBuilder;
