0736e788883aaffa62a7481c652bffa1acb3d4c2
usamahassankhan <usamahassankhan870@gmail.com>
2020-10-14 19:44:10 +0500

    evening

---
+++ burger-app/src/components/Navigation/Navigationitems/Navigationitems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import navigationItem from './Navigationitem/Navigationitem';
+import classes from './Navigationitems.css';
+import NavigationItem from './Navigationitem/Navigationitem';
+const navigationItems=()=>(
+    <ul className={classes.NavigationItems}>
+ <NavigationItem link="/" active>Burger Builder</NavigationItem>       
+<NavigationItem link="/">Checkout</NavigationItem>
+    </ul>
+);
+export default navigationItems;
