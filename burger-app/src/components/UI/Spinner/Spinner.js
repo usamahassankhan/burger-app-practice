90449e7124bcbdcc8af01b16692e1e7e69346ec3
usamahassankhan <usamahassankhan870@gmail.com>
2020-10-16 23:04:58 +0500

    aial

---
+++ burger-app/src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import classes from './Spinner.css';
+const spiiner=()=>(
+<div className={classes.Loader}>Loading...</div>
+);
+export default spinner;
--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,7 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import  BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-
+import axios from '../../axios-orders';
 const INGREDIENT_PRICES={
   salad:0.5,
   cheese:0.4,
@@ -22,8 +22,8 @@ class BurgerBuilder extends Component {
     },
     totalPrice :4,
     purchasable:false,
-    purchasing:false
-
+    purchasing:false,
+loading:false,
   }
   updatePurcahseState(ingredients){
   
@@ -57,9 +57,25 @@ this.updatePurcahseState(updatedIngredients);
     this.setState({purchasing:false});
   } 
 purchaseContinueHandler = ()=>{
-  alert("you continue");
-
-}
+  // alert("you continue");
+  const order = {
+    ingredients:this.state.ingredients,
+    price:this.state.totalPrice,
+    customer:{
+      name:'usama  hassan khan',
+      address:{
+        street:'teststreet 1',
+        zipcode:'41351',
+        country:'germany'
+      },
+      email:"uhk@gmail.com"
+    },
+    deliverymethod:'fastest'
+  }
+  axios.post('/orders.json',order)
+  .then(response=>console.log(response))
+  .catch(error=>console.log(error));
+}  
   removeIngredientHandler=(type)=>{
     
     const oldCount=this.state.ingredients[type];
